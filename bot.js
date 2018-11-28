e5a452e93593735c318d03a727fa451db67b62e0
harperb4 <44709793+harperb4@users.noreply.github.com>
2018-11-28 14:41:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,7 +83,7 @@ const messageListener = message => {
    */
   if (message['sender_type'] === 'user') {
     // Checks if the sent message contained the string 'bot'.
-    if (message['text'].indexOf('bot') !== -1) {
+    if (message['text'].indexOf('rand') !== -1) {
       /* Sends a GroupMe message as this bot to the bot's
        * group, indicating its insecurity about the topic
        * of conversation.
